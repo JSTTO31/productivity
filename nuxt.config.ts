1b9995cc6cad8215fc2ea7665652825eb4f7e76e
Joshua Sotto <joshuasottoelyen@gmail.com>
2024-07-17 20:10:15 +0800

    to ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  ssr: false,
   modules: [
     '@pinia/nuxt',
     'vuetify-nuxt-module',
